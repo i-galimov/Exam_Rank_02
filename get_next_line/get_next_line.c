164e2fe1c369ed350f2d737b54d56006a624f54c
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2021-12-25 13:11:33 +0300

    Add files via upload

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,23 @@
+#include "get_next_line.h"
+//#include <stdio.h>
+
+int get_next_line(char **line)
+{
+    int i = 0, j;
+    char *buf;
+    buf = (char *)malloc(10000);
+    while ((j = read(0, &buf[i], 1)) == 1 && buf[i] != '\n' && buf[i] != '\0')
+        i++;
+    buf[i] = '\0';
+    *line = buf;
+    return (j);
+}
+
+/* int main(void)
+{
+    int r = 0;
+    char *line;
+    while ((r = get_next_line(&line)) > 0)
+    printf("%s\n", line);
+    free(line); 
+} */
