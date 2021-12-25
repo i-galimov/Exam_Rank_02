195fa0b7b0c475bac01041289a6891935552f7fd
Ilshat Galimov <86232244+i-galimov@users.noreply.github.com>
2021-12-25 20:18:16 +0300

    Add files via upload

---
+++ inter/inter.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+
+void inter(char *str1, char *str2) {
+    int i;
+    int j;
+    int ascii[256] = {0};
+
+    i = 0;
+    while (str2[i])
+    {
+        if (ascii[(int)str2[i]] == 0)
+            ascii[(int)str2[i]] = 1;
+        i++;
+    }
+
+    i = 0;
+    j = 0;
+    while (str1[i])
+    {
+        if (ascii[(int)str1[i]] == 1)
+        {
+            ascii[(int)str1[i]] = 2;
+            write(1, &str1[i], 1);
+        }
+        i++;
+    }
+
+}
+
+int    main(int argc, char **argv)
+{
+    if (argc == 3)
+        inter(argv[1], argv[2]);
+    write(1, "\n", 1);
+    return 0;
+}
